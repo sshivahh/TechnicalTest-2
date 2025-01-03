a8a3db68bccc7185d4727abb278ffcb635cc3c42
sshivahh <russelshivah16@gmail.com>
2025-01-03 11:30:06 +0700

    login page logic done

--- app/src/main/java/com/example/technicaltest2/AuthViewModel.kt
+++ app/src/main/java/com/example/technicaltest2/AuthViewModel.kt
@@ -1,7 +1,42 @@
 package com.example.technicaltest2
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AuthViewModel : ViewModel(){
 
+    private val _authState = MutableLiveData<AuthState>().apply {
+        value = AuthState.Unauthenticated
+    }
+    val authState : LiveData<AuthState> = _authState
+
+    fun login(username: String, password: String){
+        _authState.value = AuthState.Loading
+
+        if(username.isEmpty() || password.isEmpty()){
+            _authState.value = AuthState.Error("Username and password must not be empty")
+            return
+        }
+
+        if(username == "alfagift-admin" && password == "asdf"){
+            _authState.value = AuthState.Authenticated
+        }else{
+            _authState.value = AuthState.Error("Invalid username or password")
+        }
+    }
+
+    fun resetAuthState() {
+        _authState.value = AuthState.Unauthenticated
+    }
+
+
+}
+
+sealed class AuthState {
+    data object Authenticated : AuthState()
+    data object Unauthenticated : AuthState()
+    data object Loading: AuthState()
+    data class Error(val message: String) : AuthState()
 }
--- app/src/main/java/com/example/technicaltest2/pages/LoginPage.kt
+++ app/src/main/java/com/example/technicaltest2/pages/LoginPage.kt
@@ -1,12 +1,87 @@
 package com.example.technicaltest2.pages
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.navigation.NavController
+import com.example.technicaltest2.AuthState
 import com.example.technicaltest2.AuthViewModel
 
 @Composable
 fun LoginPage(modifier: Modifier, navController: NavController, authViewModel: AuthViewModel){
-    Text("Login Page")
+    val authState = authViewModel.authState.observeAsState()
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    LaunchedEffect(authState.value) {
+        when(authState.value){
+            is AuthState.Authenticated -> {
+                navController.navigate("list")
+            }
+            is AuthState.Error -> {
+                Toast.makeText(context, (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT).show()
+                authViewModel.resetAuthState()
+            }
+            else -> { Unit }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Page")
+        Column (
+            modifier = Modifier.fillMaxWidth(0.5f),
+        ){
+            TextField(
+                value = username,
+                onValueChange = {
+                    username = it
+                },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text("Username") }
+            )
+            TextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation(),
+                label = { Text("Password") }
+            )
+            Button(
+                onClick = {
+                    authViewModel.login(username, password)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = authState.value !is AuthState.Loading
+            ) {
+                Text("Login")
+            }
+        }
+    }
 }
